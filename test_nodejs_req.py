e5f7762e3c8438e27d8351046936afab4b6678c7
Dan Callaghan <dcallagh@redhat.com>
2018-05-02 08:41:38 +1000

    properly fix nodejs.req version handling
    Rewrote the version handling logic to more closely match the grammar
    defined in: https://docs.npmjs.com/misc/semver
    
    It now passes all tests, including some outstanding broken ones.

--- test_nodejs_req.py
+++ test_nodejs_req.py
@@ -12,17 +12,17 @@ process_dep = nodejs_req.process_dep
 def test_process_dep():
     # Single version
     assert process_dep('npm(a)', '1')       == '(npm(a) >= 1 with npm(a) < 2)'
-    assert process_dep('npm(a)', '=1')      == 'npm(a) = 1'
+    assert process_dep('npm(a)', '=1')      == '(npm(a) >= 1 with npm(a) < 2)'
     assert process_dep('npm(a)', 'v1')      == '(npm(a) >= 1 with npm(a) < 2)'
-    #assert process_dep('npm(a)', '=v1')     == 'npm(a) = 1'
+    assert process_dep('npm(a)', '=v1')     == '(npm(a) >= 1 with npm(a) < 2)'
     assert process_dep('npm(a)', '1.2')     == '(npm(a) >= 1.2 with npm(a) < 1.3)'
-    assert process_dep('npm(a)', '=1.2')    == 'npm(a) = 1.2'
+    assert process_dep('npm(a)', '=1.2')    == '(npm(a) >= 1.2 with npm(a) < 1.3)'
     assert process_dep('npm(a)', 'v1.2')    == '(npm(a) >= 1.2 with npm(a) < 1.3)'
-    #assert process_dep('npm(a)', '=v1.2')   == 'npm(a) = 1.2'
-    assert process_dep('npm(a)', '1.2.3')   == 'npm(a) = 1.2.3'
-    assert process_dep('npm(a)', '=1.2.3')  == 'npm(a) = 1.2.3'
-    assert process_dep('npm(a)', 'v1.2.3')  == 'npm(a) = 1.2.3'
-    #assert process_dep('npm(a)', '=v1.2.3') == 'npm(a) = 1.2.3'
+    assert process_dep('npm(a)', '=v1.2')   == '(npm(a) >= 1.2 with npm(a) < 1.3)'
+    assert process_dep('npm(a)', '1.2.3')   == '(npm(a) >= 1.2.3 with npm(a) < 1.2.4)'
+    assert process_dep('npm(a)', '=1.2.3')  == '(npm(a) >= 1.2.3 with npm(a) < 1.2.4)'
+    assert process_dep('npm(a)', 'v1.2.3')  == '(npm(a) >= 1.2.3 with npm(a) < 1.2.4)'
+    assert process_dep('npm(a)', '=v1.2.3') == '(npm(a) >= 1.2.3 with npm(a) < 1.2.4)'
 
     # Ranges with one comparator
     assert process_dep('npm(a)', '>1')      == 'npm(a) > 1'
@@ -45,23 +45,23 @@ def test_process_dep():
     assert process_dep('npm(a)', '>=1 <=2')         == '(npm(a) >= 1 with npm(a) <= 2)'
     assert process_dep('npm(a)', '>=1.2 <=2.3')     == '(npm(a) >= 1.2 with npm(a) <= 2.3)'
     assert process_dep('npm(a)', '>=1.2.3 <=2.3.4') == '(npm(a) >= 1.2.3 with npm(a) <= 2.3.4)'
-    assert process_dep('npm(a)', '<2 >1')           == '(npm(a) < 2 with npm(a) > 1)'
-    assert process_dep('npm(a)', '<2.3 >1.2')       == '(npm(a) < 2.3 with npm(a) > 1.2)'
-    assert process_dep('npm(a)', '<2.3.4 >1.2.3')   == '(npm(a) < 2.3.4 with npm(a) > 1.2.3)'
-    assert process_dep('npm(a)', '<=2 >=1')         == '(npm(a) <= 2 with npm(a) >= 1)'
-    assert process_dep('npm(a)', '<=2.3 >=1.2')     == '(npm(a) <= 2.3 with npm(a) >= 1.2)'
-    assert process_dep('npm(a)', '<=2.3.4 >=1.2.3') == '(npm(a) <= 2.3.4 with npm(a) >= 1.2.3)'
+    assert process_dep('npm(a)', '<2 >1')           == '(npm(a) > 1 with npm(a) < 2)'
+    assert process_dep('npm(a)', '<2.3 >1.2')       == '(npm(a) > 1.2 with npm(a) < 2.3)'
+    assert process_dep('npm(a)', '<2.3.4 >1.2.3')   == '(npm(a) > 1.2.3 with npm(a) < 2.3.4)'
+    assert process_dep('npm(a)', '<=2 >=1')         == '(npm(a) >= 1 with npm(a) <= 2)'
+    assert process_dep('npm(a)', '<=2.3 >=1.2')     == '(npm(a) >= 1.2 with npm(a) <= 2.3)'
+    assert process_dep('npm(a)', '<=2.3.4 >=1.2.3') == '(npm(a) >= 1.2.3 with npm(a) <= 2.3.4)'
 
     # Hyphen ranges
     assert process_dep('npm(a)', '1.2.3 - 2.3.4')   == '(npm(a) >= 1.2.3 with npm(a) <= 2.3.4)'
-    #assert process_dep('npm(a)', '1.2.3 - 2.3')     == '(npm(a) >= 1.2.3 with npm(a) < 2.4)'
-    #assert process_dep('npm(a)', '1.2.3 - 2')       == '(npm(a) >= 1.2.3 with npm(a) < 3)'
+    assert process_dep('npm(a)', '1.2.3 - 2.3')     == '(npm(a) >= 1.2.3 with npm(a) < 2.4)'
+    assert process_dep('npm(a)', '1.2.3 - 2')       == '(npm(a) >= 1.2.3 with npm(a) < 3)'
     assert process_dep('npm(a)', '1.2 - 2.3.4')     == '(npm(a) >= 1.2 with npm(a) <= 2.3.4)'
     assert process_dep('npm(a)', '1 - 2.3.4')       == '(npm(a) >= 1 with npm(a) <= 2.3.4)'
-    #assert process_dep('npm(a)', '1.2 - 2.3')       == '(npm(a) >= 1.2 with npm(a) < 2.4)'
-    #assert process_dep('npm(a)', '1.2 - 2')         == '(npm(a) >= 1.2 with npm(a) < 3)'
-    #assert process_dep('npm(a)', '1 - 2.3')         == '(npm(a) >= 1 with npm(a) < 2.4)'
-    #assert process_dep('npm(a)', '1 - 2')           == '(npm(a) >= 1 with npm(a) < 3)'
+    assert process_dep('npm(a)', '1.2 - 2.3')       == '(npm(a) >= 1.2 with npm(a) < 2.4)'
+    assert process_dep('npm(a)', '1.2 - 2')         == '(npm(a) >= 1.2 with npm(a) < 3)'
+    assert process_dep('npm(a)', '1 - 2.3')         == '(npm(a) >= 1 with npm(a) < 2.4)'
+    assert process_dep('npm(a)', '1 - 2')           == '(npm(a) >= 1 with npm(a) < 3)'
 
     # X-Ranges
     assert process_dep('npm(a)', '1.2.x')   == '(npm(a) >= 1.2 with npm(a) < 1.3)'
@@ -74,34 +74,33 @@ def test_process_dep():
     # Tilde ranges
     assert process_dep('npm(a)', '~1.2.3')  == '(npm(a) >= 1.2.3 with npm(a) < 1.3)'
     assert process_dep('npm(a)', '~1.2.x')  == '(npm(a) >= 1.2 with npm(a) < 1.3)'
-    #assert process_dep('npm(a)', '~1.2')    == '(npm(a) >= 1.2 with npm(a) < 1.3)'
+    assert process_dep('npm(a)', '~1.2')    == '(npm(a) >= 1.2 with npm(a) < 1.3)'
     assert process_dep('npm(a)', '~1.x')    == '(npm(a) >= 1 with npm(a) < 2)'
     assert process_dep('npm(a)', '~1')      == '(npm(a) >= 1 with npm(a) < 2)'
 
     # Caret ranges
     assert process_dep('npm(a)', '^1.2.3')  == '(npm(a) >= 1.2.3 with npm(a) < 2)'
     assert process_dep('npm(a)', '^0.2.3')  == '(npm(a) >= 0.2.3 with npm(a) < 0.3)'
-    #assert process_dep('npm(a)', '^0.0.3')  == '(npm(a) >= 0.0.3 with npm(a) < 0.0.4)'
-    #assert process_dep('npm(a)', '^1.2.x')  == '(npm(a) >= 1.2 with npm(a) < 2)'
+    assert process_dep('npm(a)', '^0.0.3')  == '(npm(a) >= 0.0.3 with npm(a) < 0.0.4)'
+    assert process_dep('npm(a)', '^1.2.x')  == '(npm(a) >= 1.2 with npm(a) < 2)'
     assert process_dep('npm(a)', '^1.2')    == '(npm(a) >= 1.2 with npm(a) < 2)'
     assert process_dep('npm(a)', '^0.1.x')  == '(npm(a) >= 0.1 with npm(a) < 0.2)'
-    #assert process_dep('npm(a)', '^0.1')    == '(npm(a) >= 0.1 with npm(a) < 0.2)'
+    assert process_dep('npm(a)', '^0.1')    == '(npm(a) >= 0.1 with npm(a) < 0.2)'
     assert process_dep('npm(a)', '^1.x')    == '(npm(a) >= 1 with npm(a) < 2)'
     assert process_dep('npm(a)', '^1')      == '(npm(a) >= 1 with npm(a) < 2)'
-    assert process_dep('npm(a)', '^0.0.x')  == '(npm(a) >= 0.0 with npm(a) < 0.1)'
-    #assert process_dep('npm(a)', '^0.0')    == '(npm(a) >= 0.0 with npm(a) < 0.1)'
+    assert process_dep('npm(a)', '^0.0.x')  == 'npm(a) < 0.1'
+    assert process_dep('npm(a)', '^0.0')    == 'npm(a) < 0.1'
     assert process_dep('npm(a)', '^0.x')    == 'npm(a) < 1'
     assert process_dep('npm(a)', '^0')      == 'npm(a) < 1'
+
+    # More than two comparators in a set
+    # (no reason for this to ever appear, but it is permitted)
+    assert process_dep('npm(a)', '>1.2 <2.0 <1.9') == '(npm(a) > 1.2 with npm(a) < 1.9)'
 
     # The following cases are not implemented currently...
 
     # Multiple comparator sets separated by ||
     assert process_dep('npm(a)', '^1.2 || ^2.2') == 'npm(a)'
-
-    # More than two comparators in a set
-    # (no reason for this to ever appear, but it is permitted)
-    #assert process_dep('npm(a)', '>1.2 <2.0 <1.9') == '(npm(a) > 1.2 with npm(a) < 2.0 with npm(a) < 1.9)'
-    assert process_dep('npm(a)', '>1.2 <2.0 <1.9') == '(npm(a) > 1.2 with npm(a) < 2.0)'
 
     # The whole pre-release stuff: https://docs.npmjs.com/misc/semver#prerelease-tags
     # which is not even enumerated here because it is so complex.
